71dc2ca1e1db6eacd1e0046d25ea537cd8e544e8
kp3073 <keyurpatel8288@gmail.com>
2025-02-19 11:29:11 -0500

    argo.sh modify

--- argocd.sh
+++ argocd.sh
@@ -11,9 +11,9 @@ export PATH=/github-runner/.local/bin:/github-runner/bin:/usr/local/bin:/usr/bin
 #if account not logged in then please log in to argo cd
 argocd account list &>/dev/null
 if [ $? -ne 0 ] ; then
-argo_server_ip=$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-argo_server_password= $(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 --decode)
-argocd login ${argo_server_ip} --insecure --username admin --password ${argo_server_password}
+    argocd_ip=$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    argocd_password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o=jsonpath='{.data.password}' | base64 --decode)
+    argocd login ${argocd_ip} --insecure --username admin --password ${argocd_password}
 fi 
 
 argocd app create ${app_name} --repo https://github.com/kp3073/aks-roboshop-helm.git --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${app_name}.yaml  --path .
